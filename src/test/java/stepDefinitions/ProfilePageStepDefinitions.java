1ab5b0feab976f64b0588b3c0548f65819f8232d
akolbaev <azamat@na.edu>
2018-09-19 11:02:07 -0500

    Update ProfilePageStepDefinitions.java
    added for sample

--- src/test/java/stepDefinitions/ProfilePageStepDefinitions.java
+++ src/test/java/stepDefinitions/ProfilePageStepDefinitions.java
@@ -3,5 +3,7 @@ package stepDefinitions;
 public class ProfilePageStepDefinitions {
 	public static void main(String[] args) {
     System.out.println("Profile page");
+	System.out.println("Your are ont the profile page");
+		// sample
   }
 }
